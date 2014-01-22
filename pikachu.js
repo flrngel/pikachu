db83a2988a53cd16c483c19f4ae9e25e73f283c4
flrngel <flrngel@gmail.com>
2014-01-22 14:05:12 +0900

    pikachu test ready

--- pikachu.js
+++ pikachu.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var app=express();
+var exec=require('child_process').exec;
 
 var config=require('./config.json');
 
@@ -7,6 +8,12 @@ app.use(express.bodyParser());
 
 for( i in config.gits ){
 	app.all('/'+config.gits[i].name,function(req,res){
+		exec('git pull origin master',
+				 {cwd:gits[i].dir},
+				 function(error, stdout, stderr){
+					 console.log( {error: error, stdout: stdout, stderr: stderr } );
+				 }
+				);
 		res.send("pikachu");
 	});
 };
